b8cf0e94345d6b4b59f8391c29e003b3f69ec6ab
fracabu <100199965+fracabu@users.noreply.github.com>
2024-11-30 15:41:14 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/time-xpro-landing/', // Aggiungi questa riga
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
